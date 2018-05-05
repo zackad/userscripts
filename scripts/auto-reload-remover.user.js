94b54f3b4bd90b17594bbd7f07b9b20960924786
zackad <zackad.dev@gmail.com>
2018-05-05 12:33:40 +0700

    Create auto-reload-remover.user.js

---
+++ scripts/auto-reload-remover.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto Reload Remover
+// @namespace    https://zackad.com
+// @version      0.1
+// @description  Remove meta reload from html header
+// @author       zackad
+// @match        http://vinaget.us/check/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var meta = document.querySelector('head meta[http-equiv="refresh"]');
+    meta.parentNode.removeChild(meta);
+})();
