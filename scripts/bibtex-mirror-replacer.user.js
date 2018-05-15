9fe4574b736c28715174d9c5da44e62766703033
zackad <zackad.dev@gmail.com>
2018-05-15 08:06:11 +0700

    rename variable, fix array index access

--- scripts/bibtex-mirror-replacer.user.js
+++ scripts/bibtex-mirror-replacer.user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name         BibTex Link Replacer
 // @namespace    https://github.com/zackad
-// @version      0.1
+// @version      0.2
 // @description  Replace default url of bibtext with it's mirror
 // @author       zackad
 // @match        http://libgen.io/search.php*view=detailed*
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // @license      MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var bibtextLink = document.querySelectorAll('a[href*="/book/bibtex.php?md5="]');
-    for (var i = 0; i < bibtextLink.length; i++) {
-        var md5 = bibtextLink.getAttribute('href').split('md5=')[1];
+    var bibtexLink = document.querySelectorAll('a[href*="/book/bibtex.php?md5="]');
+    for (var i = 0; i < bibtexLink.length; i++) {
+        var md5 = bibtexLink[i].getAttribute('href').split('md5=')[1];
         var url = '/ads.php?md5=' + md5;
-        bibtextLink[i].setAttribute('href', url);
+        bibtexLink[i].setAttribute('href', url);
     }
 })();
