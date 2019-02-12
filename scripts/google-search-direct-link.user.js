4785e554c7b318e9b875a712c7051a27329bd44a
zackad <zackad.dev@gmail.com>
2019-02-12 18:49:42 +0700

    [meta] google search direct link metadata

---
+++ scripts/google-search-direct-link.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Google Search Direct Link
+// @namespace    https://github.com/zackad
+// @version      0.1.0
+// @description  Reveal original link of google search and prevent redirection.
+// @author       zackad
+// @include      http*://*.google.tld/search*
+// @run-at       document-end
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+})();
