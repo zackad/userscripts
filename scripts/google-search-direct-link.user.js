e8c6485ce4ba70fa4c0cdb43c45fbe106d1b1c6a
zackad <zackad.dev@gmail.com>
2019-02-13 10:59:12 +0700

    [release] initial version that work

--- scripts/google-search-direct-link.user.js
+++ scripts/google-search-direct-link.user.js
@@ -1,16 +1,31 @@
 // ==UserScript==
 // @name         Google Search Direct Link
 // @namespace    https://github.com/zackad
-// @version      0.1.0
+// @version      0.1.1
 // @description  Reveal original link of google search and prevent redirection.
 // @author       zackad
-// @include      http*://*.google.tld/search*
+// @include      https://*.google.tld/search*
 // @run-at       document-end
 // @license      MIT
 // ==/UserScript==
 
 (function() {
-  'use strict';
+    'use strict';
+    main();
 
-  // Your code here...
+    function getSearchResultLinks() {
+        var links = document.querySelectorAll('#rso a[onmousedown]');
+        return links;
+    }
+
+    function main() {
+        var links = getSearchResultLinks();
+        removeMouseDownAttribute(links);
+    }
+
+    function removeMouseDownAttribute(links) {
+        for (var i = 0; i < links.length; i++) {
+            links[i].removeAttribute('onmousedown');
+        }
+    }
 })();
